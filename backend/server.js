c2459fb7fa23291f01d93ed41b5f35068b592502
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2026-05-01 00:09:05 +0530

    WIP: local changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS fix for frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json());
